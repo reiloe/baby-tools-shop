3f4306433bcc86ca3318e9794a4f0785432c43d6
Reik Loeber <reik.loeber@gmail.com>
2025-09-09 15:13:44 +0200

    shellscript for running in docker container added

---
+++ babyshop_app/script.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+
+python -m venv shop-venv && source shop-venv/bin/activate
+python -m pip install pip --upgrade pip
+pip install -r requirements.txt
+
+python manage.py migrate
+
+DJANGO_SUPERUSER_PASSWORD="${DJANGO_SU_PASSWORD}" python manage.py createsuperuser --username="${DJANGO_SU_NAME}" --email="${DJANGO_SU_EMAIL}" --noinput
+
+python manage.py runserver 0:8000
