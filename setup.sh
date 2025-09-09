2a7f1f17e9655b5383d3ebbfb53f607173250b99
Reik Loeber <reik.loeber@gmail.com>
2025-09-09 17:22:17 +0200

    setup script added

---
+++ setup.sh
@@ -0,0 +1,25 @@
+#! /bin/zsh
+
+if [ "$#" -ne 1 ]
+then
+  echo "You have to input 1 Parameter:"
+  echo "Filename of environment variables"
+  echo "eg: ./setup.sh .env"
+  exit 1
+fi
+
+ENV=$1
+
+echo ##############################################
+echo #            Create Docker Image             #
+echo ##############################################
+docker build -t babyshop .
+
+echo ##############################################
+echo #          Starting Docker Container         #
+echo ##############################################
+docker run --name babyshop -d -p 8025:8000  --env-file="${ENV}" babyshop
+
+echo ##############################################
+echo #                  Finished                  #
+echo ##############################################
